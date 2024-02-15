ec323c3c6f802dcac6a2bd9fe6e7bfad36cf3654
adiadishr <146527912+adiadishr@users.noreply.github.com>
2024-02-16 02:57:28 +0545

    store page

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,10 @@
-document.getElementById("topbutton").addEventListener("click", function(event) {
+// Smooth Scroll with even slower speed
+document.getElementById("topbutton").addEventListener("click", function (event) {
     event.preventDefault();
     const navbar = document.getElementById("navbar");
-    navbar.scrollIntoView({ behavior: "smooth" });
+
+    // Delay scrolling by 100 milliseconds (adjust as needed)
+    setTimeout(function () {
+        navbar.scrollIntoView({ behavior: "smooth", block: "start", inline: "nearest" }); // Adjust the options here
+    }, 100); // Adjust the delay time here
 });
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,13 +5,19 @@ module.exports = {
     extend: {
       colors: {
         primary: '#4D7C0F',
-        background:'#FAFDF2',
-        heading:'#1A2E05',
+        background: '#FAFDF2',
+        heading: '#1A2E05',
         textcolor: '#262626',
         light: '#CBEA7B',
       },
       fontFamily: {
-        'universal':['Poppins']
+        'universal': ['Poppins']
+      },
+      boxShadow: {
+        'card': '-5px 4px 4px rgba(0, 0, 0, 0.25)',
+      },
+      screens: {
+        '1080px': '1080px',
       },
     },
   },
